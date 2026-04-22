74c95f4867f166998767aa4a82fff682b124b2f9
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:03:21 -0500

    TUnit conversion

--- test/Soenneker.Mailgun.HttpClients.Tests/MailgunOpenApiHttpClientTests.cs
+++ test/Soenneker.Mailgun.HttpClients.Tests/MailgunOpenApiHttpClientTests.cs
@@ -1,20 +1,19 @@
 using Soenneker.Mailgun.HttpClients.Abstract;
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Mailgun.HttpClients.Tests;
 
-[Collection("Collection")]
-public sealed class MailgunOpenApiHttpClientTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public sealed class MailgunOpenApiHttpClientTests : HostedUnitTest
 {
     private readonly IMailgunOpenApiHttpClient _httpclient;
 
-    public MailgunOpenApiHttpClientTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public MailgunOpenApiHttpClientTests(Host host) : base(host)
     {
         _httpclient = Resolve<IMailgunOpenApiHttpClient>(true);
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
